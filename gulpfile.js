bca5763c7b56b65352c8d541b032274ae461eec2
Youri Ackx <youri@ackx.net>
2018-05-04 11:33:33 +0200

    Re-arrange project structure and gulp it

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+var banner = ['/*!\n',
+  ' * solar-webgl - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2014-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' */\n',
+  ''
+].join('');
+
+gulp.task('dist', ['default'], function () {
+    gulp.src(['index.html',]).pipe(gulp.dest('./dist'));
+    gulp.src(['app/img/*']).pipe(gulp.dest('./dist/img'));
+});
+
+// Default task
+gulp.task('default', ['dist']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+// Dev task
+gulp.task('dev', ['browserSync'], function() {
+  gulp.watch('./img/*', ['img']);
+  gulp.watch('./*.html', browserSync.reload);
+});
